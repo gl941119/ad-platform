e69b173f3af527a69a19ff0a98a6f89bfd8375fe
yuqian <yuqian@bierinc.com>
2018-06-29 15:19:49 +0800

    add crowdsale component

---
+++ src/components/index-com/crowdsale-item.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="crowdsale-item">
+        <div class="crowdsale-item-title">
+            <span>品牌规划的投入和策略</span>
+            <span class="crowdsale-item-title-border"></span>
+            <span class="crowdsale-item-title-ad">广告</span>
+        </div>
+        <p class="crowdsale-item-sn">
+            <span>项目编号：</span>
+            <span>BR1806536</span>
+        </p>
+        <p class="crowdsale-item-abstract">
+            通常情况下，高端品牌的优势都是比较大的，这也为企业的经营和发展带来了很大的优势，不过，还是要充分考虑到品牌形象的维护，因为这对企业的发展所起到的积极作用是不言而喻的。
+        </p>
+        <div class="crowdsale-item-list">
+            <div class="crowdsale-item-list-left">
+                <div class="crowdsale-item-list-left-label">
+                    <span>2018世界杯</span>
+                </div>
+                <div class="crowdsale-item-list-left-label">
+                    <span>超级去中心化式交易</span>
+                </div>
+                <div class="crowdsale-item-list-left-label">
+                    <span>Dapp去中心化</span>
+                </div>
+                <div class="crowdsale-item-list-left-label">
+                    <span>比特币山寨</span>
+                </div>
+                <div class="crowdsale-item-list-left-label">
+                    <span>本轮发行：</span>
+                    <span>21,000,000</span>
+                </div>
+            </div>
+            <div class="crowdsale-item-list-right">
+                <div v-show="!isEventOver" :class="{'event-over': isEventOver}" class="crowdsale-item-list-right-content">
+                    <div>剩余时间</div>
+                    <div class="crowdsale-item-list-right-content-divide"></div>
+                    <div>12:07:18</div>
+                </div>
+                <div v-show="isEventOver" :class="{'event-over': isEventOver}" class="crowdsale-item-list-right-content">
+                    已结束
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['isEventOver'],
+    data (){
+        return {}
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../../assets/css/variable.scss';
+@import '../../assets/css/global.scss';
+.crowdsale-item {
+    padding: 16px;
+    width: crowdsaleItemWidth;
+    background: $crowdsaleBackGround;
+    margin-bottom: 14px;
+    &-title {
+        @include content-flex();
+        height: 25px;
+        font-weight: bold;
+        font-size: $headerFontSize;
+        color: $crowdsaleTitleColor;
+        &-border {
+            height: 100%;
+            border: 1px solid #E2E2E2;
+            margin-left: 32px;
+            margin-right: 10px;
+        }
+        &-ad {
+           font-size: $crowdsaleAdFontSize;
+           color: #000;
+           font-weight: normal; 
+        }
+    }
+    &-sn {
+        color: $crowdsaleSNColor;
+        font-size: $crowdsaleAdFontSize;
+        margin-bottom: 10px;
+        line-height: 17px;
+    }
+    &-abstract {
+        color: $crowdsaleAbstractColor;
+        font-size: $crowdsaleAdFontSize;
+        line-height: 20px;
+    }
+    &-list {
+        @include content-flex();
+        height: 73px;
+        margin-top: 32px;
+        &-left {
+            width: 226px;
+            height: 100%;
+            @include content-flex();
+            flex-wrap: wrap;
+            &-label {
+                width: 112px;
+                background: #E0E0E0;
+                border-radius: 3px;
+                text-align: center;
+                margin-bottom: 5px;
+                & span {
+                    font-size: 12px;
+                    display: inline-block;
+                    transform: scale(0.85);
+                }
+                &:nth-child(2n) {
+                    margin-left: 2px;
+                }
+                &:last-child {
+                    width: 100%;
+                    margin: 0;
+                }
+            }
+        }
+        &-right {
+            height: 100%;
+            margin-left: 21px;
+            flex: 1;
+            &-content {
+                height: 100%;
+                background: #FF9500;
+                @include content-flex(center);
+                flex-direction: column;
+                color: #fff;
+                border-radius: 5px;
+                font-size: 18px;
+                &-divide {
+                    width: 63px;
+                    height: 1px;
+                    background: #fff;
+                    margin: 5px 0;
+                }
+                &.event-over {
+                    background: #999999;
+                }
+            }
+        }
+    }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,16 +1,71 @@
 <template>
-  <div>
+  <div class="platform-index">
       <custom-carousel></custom-carousel>
+      <div class="platform-index-bull">
+          <div class="platform-index-bull-block">行情播报</div>
+          <div class="platform-index-bull-content"></div>
+      </div>
+      <div class="platform-index-item">
+          <div class="platform-index-item-crowdsale">
+              <div class="platform-index-item-crowdsale-title">众筹</div>
+              <crowdsale-item :is-event-over="true" ></crowdsale-item>
+              <crowdsale-item :is-event-over="false" ></crowdsale-item>
+          </div>
+          <div class="plat-index-item-ad"></div>
+      </div>
   </div>
 </template>
 <script>
-import customCarousel from '@/components/custom-carousel';
+    import customCarouselCom from '@/components/custom-carousel';
+    import crowdsaleItemCom from '@/components/index-com/crowdsale-item';
     export default {
         data() {
             return {}
         },
         components: {
-            'custom-carousel': customCarousel,
+            'custom-carousel': customCarouselCom,
+            'crowdsale-item': crowdsaleItemCom,
         }
     }
 </script>
+<style lang="scss" scoped>
+    @import '../../assets/css/variable.scss';
+    @import '../../assets/css/global.scss';
+    %platform-index-title {
+        font-size: $crowdsaleFontSize;
+        color: $crowdsaleFontColor;
+        font-weight: bold;
+        font-family: semibold;
+        line-height: 28px;
+        padding: 17px 0 10px;
+    }
+
+    .platform-index {
+        &-bull {
+            height: $bullHeight;
+            @include body-center();
+            @include content-flex();
+            border-bottom: 1px solid #e1e1e1;
+            border-right: 1px solid #e1e1e1;
+            &-block {
+                width: $bullBlockWidth;
+                height: 100%;
+                background: $bullBlockColor;
+                @include content-flex(center);
+                font-size: $headerFontSize;
+                color: #e5e5e5;
+                box-shadow: 3px 3px 10px $bullBlockColor;
+            }
+        }
+        &-item {
+            @include body-center();
+            @include content-flex();
+            &-crowdsale {
+                width: $crowdsaleWidth;
+                &-title {
+                    @extend %platform-index-title;
+                }
+            }
+        }
+    }
+</style>
