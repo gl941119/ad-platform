00cf081528d85d97f04f91b0830b7597666608cd
yuqian <yuqian@bierinc.com>
2018-07-07 16:20:58 +0800

    head portrait

--- src/utils/config.js
+++ src/utils/config.js
@@ -8,4 +8,21 @@ export default {
     pageSize: 10, // 一页的数量
     pageStart: 1,
     TelegramBot: 'hanguishuai_bot',
+    headPortrait: [
+        'http://pic.bierinc.com/chainadvert/1@1530951208714.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208692.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208683.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208691.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208683.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208716.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208824.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208821.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208811.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208850.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208829.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208908.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208929.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208942.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+        'http://pic.bierinc.com/chainadvert/1@1530951208936.svg?x-bier.oss-process=image/resize,m_fixed,w_250',
+    ],
 };
