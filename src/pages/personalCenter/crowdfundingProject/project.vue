b27f660a04d192ecb7a27682f60e0e88e1f40f3d
gaigai <gaigaiyang@bierinc.com>
2018-07-20 11:44:58 +0800

    button style

--- src/pages/personalCenter/crowdfundingProject/project.vue
+++ src/pages/personalCenter/crowdfundingProject/project.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="my_crowdfunding">
 		<div class="advertising_revenu_account_flow">
-			<p class="my_crowdfunding_title">{{$t('initiated.iInitiated')}}</p>
-			<router-link tag="p" :to="{ name: 'newCrowdfunding'}" class="my_crowdfunding_title_intro">{{$t('initiated.newCrowd')}}</router-link>
+			<div class="my_crowdfunding_title">
+				<p class="my_crowdfunding_title_info">{{$t('initiated.iInitiated')}}</p>
+				<el-button class="my_crowdfunding_title_button" @click="newProject()" type="text">{{$t('initiated.newCrowd')}}
+				</el-button>
+			</div>
 			<el-table :data="crowdfundingData" style="width: 100%" @sort-change="sortChange" :default-sort="{prop: 'date', order: 'descending'}">
 				<el-table-column prop="shotEnName" :label="$t('initiated.token')">
 				</el-table-column>
@@ -62,6 +65,11 @@
 			}
 		},
 		methods: {
+			newProject(){
+				this.$router.push({
+					name: 'newCrowdfunding',
+				});
+			},
 			crowdfunding() {
 				Request({
 					url: 'QueryMyNewCrowdfunding',
@@ -104,16 +112,28 @@
 	background: #FFFFFF;
 	padding: 20px;
 	&_title{
-		font-size: 24px;
-		&_intro {
+		&_info{
+			font-size: 24px;
+		}
+		&_button {
 			margin-bottom: 30px;
 			float: right;
 			font-size:18px;
-			color:rgba(153,153,153,1);
+			color:rgba(255,255,255,1);
 			line-height:25px;
+			background:rgba(255,149,0,1);
+			border-radius:4px;
+			padding: 5px 20px;
 		}
 	}
-	
+	.ad-serving_title_button{
+			font-size:18px;
+			color:rgba(255,255,255,1);
+			line-height:25px;
+			background:rgba(255,149,0,1);
+			border-radius:4px;
+			padding: 5px 20px;
+	}
 	.my_crowdfunding_data_pages {
 		margin-top: 30px;
 		text-align: center;
